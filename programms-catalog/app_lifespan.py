918d7d3f6a64bf9ac6e1e6eef649b412f9f8d45c
Bulstray <96692002+Bulstray@users.noreply.github.com>
2025-10-17 14:40:16 +0300

    set state in lifespan

--- programms-catalog/app_lifespan.py
+++ programms-catalog/app_lifespan.py
@@ -1,11 +1,16 @@
 from collections.abc import AsyncIterator
 from contextlib import asynccontextmanager
 
+from core.config import settings
 from fastapi import FastAPI
+from storage.program import ProgramsStorage
 
 
 @asynccontextmanager
-async def lifespan(
-    app: FastAPI,
-) -> AsyncIterator[None]:
+async def lifespan(app: FastAPI) -> AsyncIterator[None]:
+
+    app.state.programs_storage = ProgramsStorage(
+        hash_name=settings.redis.collection.program_hash,
+    )
+
     yield
