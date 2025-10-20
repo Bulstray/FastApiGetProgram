0416a487da7d7ac687405dcaa3e67377cb27f9e1
Bulstray <96692002+Bulstray@users.noreply.github.com>
2025-10-20 15:21:09 +0300

    create main_file

--- programms-catalog/main.py
+++ programms-catalog/main.py
@@ -1,7 +1,17 @@
+import uvicorn
+
 from app_lifespan import lifespan
 from fastapi import FastAPI
+
+from api.api_v1.programs.views.list_views import router
 
 app = FastAPI(
     title="Programs",
     lifespan=lifespan,
 )
+
+app.include_router(router)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app)
