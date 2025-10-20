48863935b06c1c2bba4e236e48492164953c37d7
Bulstray <96692002+Bulstray@users.noreply.github.com>
2025-10-20 15:18:44 +0300

    init create and get_program_list

---
+++ programms-catalog/api/api_v1/programs/views/list_views.py
@@ -0,0 +1,29 @@
+from fastapi import APIRouter, status, UploadFile, File, Form
+
+from schemas.programs.programs import Program, ProgramCreate, ProgramRead
+from storage.program.crud import storage
+
+
+router = APIRouter(prefix="/programs", tags=["Programs"])
+
+
+@router.get("/", response_model=list[ProgramRead])
+def read_program_list() -> list[Program]:
+    return storage.get()
+
+
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=Program)
+def add_program(
+    file: UploadFile,
+    name: str = Form(...),
+    description: str = Form(...),
+):
+    program_create = ProgramCreate(
+        name=name,
+        description=description,
+    )
+
+    storage.create_or_raise_if_exists(
+        program=program_create,
+        file=file,
+    )
