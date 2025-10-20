ada91dbf54b080aef969ac6b420d07b0901007e2
Ramil Khakimov <bulstray@yandex.ru>
2025-10-20 22:10:05 +0300

    create dependensies

--- programms-catalog/dependensies/programs.py
+++ programms-catalog/dependensies/programs.py
@@ -1,7 +1,35 @@
-from fastapi import FastAPI, Depends, Request
+from typing import Annotated
+
+from fastapi import Depends, Request, status, HTTPException
 
 from storage.program.crud import ProgramsStorage
 
+from schemas.programs.programs import Program
+
 
 def get_programs_storage(request: Request) -> ProgramsStorage:
-    pass
+    return request.app.state.programs_storage
+
+
+GetProgramsStorage = Annotated[
+    ProgramsStorage,
+    Depends(get_programs_storage),
+]
+
+
+def prefetch_program_by_name(
+    name: str,
+    storage: GetProgramsStorage,
+) -> Program:
+    program: Program | None = storage.get_by_name(name=name)
+
+    if program:
+        return program
+
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Program not found",
+    )
+
+
+ProgramByName = Annotated[Program, Depends(prefetch_program_by_name)]
