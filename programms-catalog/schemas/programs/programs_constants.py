d683fc73f2a22a2521e2b1bc984e035a3d547b50
Bulstray <96692002+Bulstray@users.noreply.github.com>
2025-10-20 10:33:23 +0300

    refactor string types

--- programms-catalog/schemas/programs/programs_constants.py
+++ programms-catalog/schemas/programs/programs_constants.py
@@ -1,4 +1,21 @@
+from typing import Annotated
+
+from annotated_types import Len, MaxLen
+
+# Limitations for the scheme
 NAME_MIN_LENGTH = 3
 NAME_MAX_LENGTH = 50
 
 DESCRIPTION_MAX_LENGTH = 200
+
+
+# String types with length constraints for the schema
+NameString = Annotated[
+    str,
+    Len(
+        min_length=NAME_MIN_LENGTH,
+        max_length=NAME_MAX_LENGTH,
+    ),
+]
+
+DescriptionString = Annotated[str, MaxLen(DESCRIPTION_MAX_LENGTH)]
