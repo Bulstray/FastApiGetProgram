18f44b06e4fd07e5da8100f4c7dfd880f3ccee36
Bulstray <96692002+Bulstray@users.noreply.github.com>
2025-10-20 15:19:46 +0300

    update schemas

--- programms-catalog/schemas/programs/programs.py
+++ programms-catalog/schemas/programs/programs.py
@@ -1,8 +1,4 @@
-from pathlib import Path
-
-from pydantic import BaseModel, field_validator
-
-from schemas.programs.exceptions import UnsupportedFormatFileError
+from pydantic import BaseModel
 from schemas.programs.programs_constants import DescriptionString, NameString
 
 
@@ -10,19 +6,13 @@ class ProgramBase(BaseModel):
     name: NameString
     description: DescriptionString
 
-    file_path: Path
 
-    @field_validator("file_path")
-    def validate_file_path(self, v: Path) -> Path:
-        archive_extensions: set[str] = {".zip", ".7z", ".rar"}
-
-        if v.suffix.lower() not in archive_extensions:
-            raise UnsupportedFormatFileError
-        return v
+class ProgramRead(ProgramBase):
+    """Модель для просмотра программ"""
 
 
 class Program(ProgramBase):
-    """Модель для хранения данных фильма"""
+    """Модель для хранения данных программ"""
 
 
 class ProgramCreate(ProgramBase):
