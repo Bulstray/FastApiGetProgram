241c9c71528793c070dc95e8d51446cf09211d1f
Ramil Khakimov <bulstray@yandex.ru>
2025-10-20 22:09:50 +0300

    add delete views

--- programms-catalog/api/api_v1/programs/views/details_views.py
+++ programms-catalog/api/api_v1/programs/views/details_views.py
@@ -1,9 +1,22 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
 
+from dependensies.programs import ProgramByName, GetProgramsStorage
+from schemas.programs.programs import ProgramRead
 
 router = APIRouter()
 
 
-@router.get("/program/{name}/")
-def get_program_by_name(name: str):
+@router.get(
+    "/program/{name}/",
+    response_model=ProgramRead,
+)
+def get_program_by_name(name: ProgramByName):
+    return name
+
+
+@router.delete(
+    "/program/{name}/",
+    status_code=status.HTTP_204_NO_CONTENT,
+)
+def delete_program_by_name(name: ProgramByName):
     pass
