0d4291cf2da2bfdc6549ca34627ddd0cdba2743b
Bulstray <96692002+Bulstray@users.noreply.github.com>
2025-10-21 10:22:32 +0300

    fix for ruff and mypy

--- programms-catalog/storage/program/crud.py
+++ programms-catalog/storage/program/crud.py
@@ -1,16 +1,15 @@
-import os
 import shutil
-
-from fastapi import UploadFile
+from collections.abc import Iterable
 from pathlib import Path
-
-from pydantic import BaseModel
-from redis import Redis
-
-from schemas.programs.programs import ProgramCreate, Program
+from typing import cast
 
 from core.config import settings
-from .exceptions import UnsupportedFormatFileError, ProgramAlreadyExistsError
+from fastapi import UploadFile
+from pydantic import BaseModel
+from redis import Redis
+from schemas.programs.programs import Program, ProgramCreate
+
+from .exceptions import ProgramAlreadyExistsError, UnsupportedFormatFileError
 
 redis = Redis(
     host=settings.redis.connection.host,
@@ -29,7 +28,7 @@ class ProgramsStorage(BaseModel):
 
         file_path = UPLOAD_DIR / f"{program.name}.zip"
 
-        with open(file_path, "wb") as buffer:
+        with Path.open(file_path, "wb") as buffer:
             shutil.copyfileobj(file.file, buffer)
 
         redis.hset(
@@ -40,18 +39,23 @@ class ProgramsStorage(BaseModel):
         return Program(**program.model_dump())
 
     def __exists(self, name: str) -> bool:
-        return redis.hexists(
-            name=self.hash_name,
-            key=name,
+        return cast(
+            bool,
+            redis.hexists(
+                name=self.hash_name,
+                key=name,
+            ),
         )
 
     def create_or_raise_if_exists(
-        self, program: ProgramCreate, file: UploadFile
+        self,
+        program: ProgramCreate,
+        file: UploadFile,
     ) -> None | Program:
         if self.__exists(name=program.name):
             raise ProgramAlreadyExistsError(program.name)
 
-        if not file.filename.endswith(".zip"):
+        if not cast(str, file.filename).endswith(".zip"):
             raise UnsupportedFormatFileError(file.filename)
 
         return self.__save_program(program=program, file=file)
@@ -59,14 +63,17 @@ class ProgramsStorage(BaseModel):
     def get(self) -> list[Program]:
         return [
             Program.model_validate_json(json_data=json_data)
-            for json_data in redis.hvals(
-                name=settings.redis.collection.program_hash,
+            for json_data in cast(
+                Iterable[str],
+                redis.hvals(
+                    name=settings.redis.collection.program_hash,
+                ),
             )
         ]
 
     def get_by_name(self, name: str) -> Program | None:
         if answer := redis.hget(name=self.hash_name, key=name):
-            return Program.model_validate_json(answer)
+            return Program.model_validate_json(cast(str, answer))
 
         return None
 
